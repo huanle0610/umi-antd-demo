3f4f548144144441004210169ec269648da7f6e1
lizhijiang <huanle0610@qq.com>
2023-06-02 11:08:22 +0800

    add products

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+    plugins: ['@umijs/plugins/dist/react-query'],
+    reactQuery: {},
   routes: [
     { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+        { path: "/docs", component: "docs" },
+    { path: "/products", component: "products" },
   ],
   npmClient: 'pnpm',
 });
